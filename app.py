042e3947f3cb582fdceb5309f15387d299d65d4e
Muhamad Farrel Rizqullah <92018210+muhamadfarrelrizqullah@users.noreply.github.com>
2024-06-28 12:05:00 +0700

    add: migration

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import Flask, request, jsonify
 from sqlalchemy import text
 from config import Config
 from models import db, User
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
+migrate = Migrate(app, db)
 
 # Tes Koneksi
 @app.route('/test-connection', methods=['GET'])
